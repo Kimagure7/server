bab7c681724d1b8b2bfb1933394662b3c889656b
Kimagure <re2477036@mail.ustc.edu.cn>
2022-06-27 20:01:45 +0800

    重命名

--- main_server/pytoneo.py
+++ main_server/pytoneo.py
@@ -1,4 +1,5 @@
 import logging
+import queue
 import string
 from neo4j import GraphDatabase
 from neo4j.exceptions import ServiceUnavailable
@@ -58,6 +59,10 @@ class App:
     def delete_all(self):
         with self.driver.session() as session:
             session.write_transaction(self._delete_all)
+            
+    def rename_node(self,node):
+        with self.driver.session() as session:
+            session.write_transaction(self._rename_node,node)
 
     @staticmethod
     def _createnamenode(tx,id):
@@ -74,7 +79,7 @@ class App:
     def _create_and_return_relationship(tx, labelname, id,owner):
         query = (
             "MATCH (n:FILE) WHERE n.id="+str(id)+
-            " MATCH (m:Label) WHERE m.name=\'"+ labelname+"\' , m.owner = \'"+owner+"\'" 
+            " MATCH (m:Label) WHERE m.name=\'"+ labelname+"\' and m.owner = \'"+owner+"\'" 
             "CREATE (n)-[r1:tag]->(m) "
             # "CREATE (m)-[r2:tag"+"]->(n) "
             "RETURN n,m"
@@ -109,7 +114,10 @@ class App:
             "WHERE p.name=\'"+labelname+"\' RETURN p"
         )
         result = tx.run(query)
+        # print(result)
+        # print([record["p"] for record in result])
         return [record["p"] for record in result]
+        
 
     @staticmethod
     def _delete_node(tx, deletenode):
@@ -120,7 +128,7 @@ class App:
         path = deletenode['path']
         owner = deletenode['owner']
         query = (
-            "MATCH (p:FILE{nodename:\""+nodename+"\", ext = \""+nodeext+"\", path = \""+path+"\",owner = \""+owner+"\"})" 
+            "MATCH (p:FILE{name:\""+nodename+"\", ext: \""+nodeext+"\", path: \""+path+"\",owner: \""+owner+"\"})" 
             " DETACH DELETE p"
             
         )
@@ -134,19 +142,63 @@ class App:
     @staticmethod
     def _delete_all(tx):
         tx.run("match (n) detach delete n")
+        
+    @staticmethod
+    def _rename_node(tx,node):
+        nodename_split = node['nodename'].split('.')
+        nodename = nodename_split[0]
+        nodeext = nodename_split[1]
+        path = node['path']
+        owner = node['owner']
+        newname_split = node['newname'].split('.')
+        newname = newname_split[0]
+        newext = newname_split[1]
+        # 修改标签 有一个与源文件同名的标签得修改
+        # 第一步 删除tag
+        query = (
+            "MATCH (p:FILE{name:\""+nodename+"\", ext: \""+nodeext+"\", path: \""+path+"\",owner: \""+owner+"\"})"
+            "-[r]-(n:Label{name:\""+nodename+"\"})"
+            " delete r"
+        )
+        tx.run(query)
+        # 记得删除孤立节点
+        query = (
+            "MATCH (n) where not exists((n)--()) detach delete n"
+        )
+        tx.run(query)
+        # 第三步 修改节点属性
+        query = (
+            "MATCH (p:FILE{name:\""+nodename+"\", ext: \""+nodeext+"\", path: \""+path+"\",owner: \""+owner+"\"})"
+            "set p.name = \""+newname+"\", p.ext = \""+newext+"\""
+        )
+        tx.run(query)
+        # 第四步 新建该文件名的标签 加关系
+        query = (
+            "MERGE (p1:Label{ name: $labelname , owner: $owner}) "
+            "RETURN p1"
+        )
+        tx.run(query, labelname=newname,owner=owner)
+        # 第五步 加关系
+        query = (
+            "MATCH (p:FILE{name:\""+newname+"\", ext: \""+newext+"\", path: \""+path+"\",owner: \""+owner+"\"})"
+            " MATCH (m:Label) WHERE m.name=\'"+ newname +"\' and m.owner = \'"+owner+"\'" 
+            " CREATE (p)-[r1:tag]->(m) "
+            "RETURN p,m"
+        )
+        tx.run(query)
 
 if __name__ == "__main__":
     #端口名、用户名、密码根据需要改动
     #create_newnode(node)用于创建结点（包括检测标签、创建标签节点、添加相应的边等功能）
     #delete_node(node.name)用于删去名为node.name的结点
     scheme = "bolt"  # Connecting to Aura, use the "neo4j+s" URI scheme
-    host_name = "localhost"
+    host_name = "101.33.236.114"
     port = 7687
-    url = "bolt://localhost:7687".format(scheme=scheme, host_name=host_name, port=port)
+    url = "{scheme}://{host_name}:7687".format(scheme=scheme, host_name=host_name, port=port)
     user = "neo4j"
-    password = "disgrafs"
-    
+    password = "11"
+    node = {"nodename":"ipadpro.pdf","path":"home/","owner":"zzy","newname":"hi"}
     app = App(url, user, password)
-    app.delete_node("USTCHORUSCAT.TXT".lower())
+    app.rename_node(node)
     app.close()
     
